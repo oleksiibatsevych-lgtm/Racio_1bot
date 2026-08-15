d39414c7b2ea4e207410556a987543e4cec97f55
oleksiibatsevych-lgtm <oleksiibatsevych@gmail.com>
2026-08-15 08:09:22 +0300

    Add configuration settings for trading bot
    Added configuration for Telegram token, render URL, database file, and currency pairs mapping.

--- config.py
+++ config.py
@@ -1,1 +1,31 @@
+import os
 
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+RENDER_URL = os.environ.get(
+    "RENDER_EXTERNAL_URL", "https://racio-1bot.onrender.com"
+)
+DB_FILE = "trading_bot.db"
+
+PAIRS_MAP = {
+    "CHF/JPY": "CHFJPY=X",
+    "AUD/CAD": "AUDCAD=X",
+    "GBP/AUD": "GBPAUD=X",
+    "EUR/USD": "EURUSD=X",
+    "EUR/CAD": "EURCAD=X",
+    "AUD/USD": "AUDUSD=X",
+    "AUD/CHF": "AUDCHF=X",
+    "CAD/CHF": "CADCHF=X",
+    "EUR/CHF": "EURCHF=X",
+    "GBP/CHF": "GBPCHF=X",
+    "USD/CAD": "USDCAD=X",
+    "GBP/USD": "GBPUSD=X",
+    "GBP/JPY": "GBPJPY=X",
+    "EUR/AUD": "EURAUD=X",
+    "CAD/JPY": "CADJPY=X",
+    "USD/CHF": "USDCHF=X",
+    "EUR/GBP": "EURGBP=X",
+    "USD/JPY": "USDJPY=X",
+    "AUD/JPY": "AUDJPY=X",
+    "EUR/JPY": "EURJPY=X",
+    "GBP/CAD": "GBPCAD=X",
+}
