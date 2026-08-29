a7a7dfcd34b01d4e4ea12d1d479cd301bdc7240d
oleksiibatsevych-lgtm <oleksiibatsevych@gmail.com>
2026-08-29 10:54:52 +0300

    Update TELEGRAM_TOKEN and PAIRS_MAP in config.py
    Updated TELEGRAM_TOKEN default value and modified PAIRS_MAP to include additional currency pairs.

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
-RENDER_URL = os.environ.get("RENDER_EXTERNAL_URL", "https://racio-1bot.onrender.com")
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "YOUR_TELEGRAM_TOKEN")
 
 PAIRS_MAP = {
     "EUR/USD": "EURUSD=X",
@@ -10,19 +9,19 @@ PAIRS_MAP = {
     "AUD/USD": "AUDUSD=X",
     "USD/CAD": "USDCAD=X",
     "USD/CHF": "USDCHF=X",
-    "EUR/GBP": "EURGBP=X",
     "EUR/JPY": "EURJPY=X",
     "GBP/JPY": "GBPJPY=X",
     "AUD/JPY": "AUDJPY=X",
     "CAD/JPY": "CADJPY=X",
     "CHF/JPY": "CHFJPY=X",
+    "EUR/GBP": "EURGBP=X",
     "EUR/AUD": "EURAUD=X",
     "EUR/CAD": "EURCAD=X",
+    "EUR/CHF": "EURCHF=X",
     "GBP/AUD": "GBPAUD=X",
     "GBP/CAD": "GBPCAD=X",
+    "GBP/CHF": "GBPCHF=X",
     "AUD/CAD": "AUDCAD=X",
     "AUD/CHF": "AUDCHF=X",
-    "CAD/CHF": "CADCHF=X",
-    "EUR/CHF": "EURCHF=X",
-    "GBP/CHF": "GBPCHF=X",
+    "CAD/CHF": "CADCHF=X"
 }
